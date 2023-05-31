c2d9152cb92a1355f68c9b8b0eee154ac4fe092e
Stepp1 <sfaragg@gmail.com>
2023-05-31 14:45:49 -0400

    dataset utils

--- generalization/randomization/__init__.py
+++ generalization/randomization/__init__.py
@@ -1,42 +1,25 @@
-from generalization.randomization.dataset import RandomizedDataset
-from torchvision.datasets import CIFAR10, ImageNet
+from .corruptions import gaussian_pixels, random_labels, random_pixels, shuffled_pixels
+from .dataset import RandomizedDataset
+from .utils import build_cifar10, create_corrupted_dataset
 
 
 def available_corruptions():
     return [
-        "random_labels",
-        "partial_labels",
         "gaussian_pixels",
+        "random_labels",
         "random_pixels",
+        "partial_labels",
         "shuffled_pixels",
     ]
 
 
-def create_corrupted_dataset(
-    dataset_name,
-    corruption_name,
-    corruption_prob=0.3,
-    train=True,
-    root="./data/cifar10",
-    apply_corruption=False,
-    return_corruption=False,
-    transform=None,
-    target_transform=None,
-):
-    if dataset_name.lower() == "imagenet":
-        dataset = ImageNet(root=root, download=True, train=train)
-    elif dataset_name.lower() == "cifar10":
-        dataset = CIFAR10(root=root, download=True, train=train)
-    else:
-        raise ValueError("Dataset name must be either 'imagenet' or 'cifar10'")
-
-    return RandomizedDataset(
-        dataset,
-        corruption_name=corruption_name,
-        corruption_prob=corruption_prob,
-        apply_corruption=apply_corruption,
-        return_corruption=return_corruption,
-        train=train,
-        transform=transform,
-        target_transform=target_transform,
-    )
+__all__ = [
+    "available_corruptions",
+    "build_cifar10",
+    "create_corrupted_dataset",
+    "RandomizedDataset",
+    "gaussian_pixels",
+    "random_labels",
+    "random_pixels",
+    "shuffled_pixels",
+]
