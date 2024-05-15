e1301bdc3d2462c480d02d78e0c42e135d4dbece
Stepp1 <sfaragg@gmail.com>
2024-05-15 16:34:42 -0400

    run script + reqs

--- generalization/scripts/run_configs.sh
+++ generalization/scripts/run_configs.sh
@@ -6,15 +6,16 @@ file_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $file_dir; cd ../
 
 # python ./utils/train.py --config ./configs/cifar-normal_labels.yaml 
+# python ./utils/train.py --config ./configs/cifar-inception-gaussian_pixels.yaml  
+# python ./utils/train.py --config ./configs/cifar-inception-shuffled_pixels.yaml  
+# python ./utils/train.py --config ./configs/cifar-inception-partial_labels.yaml   
+
+# python ./utils/train.py --config ./configs/cifar-resnet-gaussian_pixels.yaml  
+# python ./utils/train.py --config ./configs/cifar-resnet-shuffled_pixels.yaml
+# python ./utils/train.py --config ./configs/cifar-resnet-partial_labels.yaml
 
 python ./utils/train.py --config ./configs/cifar-alexnet-gaussian_pixels.yaml  
-python ./utils/train.py --config ./configs/cifar-alexnet-shuffled_pixels.yaml  
+# python ./utils/train.py --config ./configs/cifar-alexnet-shuffled_pixels.yaml  
 python ./utils/train.py --config ./configs/cifar-alexnet-partial_labels.yaml  
 
-python ./utils/train.py --config ./configs/cifar-inception-gaussian_pixels.yaml  
-python ./utils/train.py --config ./configs/cifar-inception-shuffled_pixels.yaml  
-python ./utils/train.py --config ./configs/cifar-inception-partial_labels.yaml   
 
-python ./utils/train.py --config ./configs/cifar-resnet-gaussian_pixels.yaml  
-python ./utils/train.py --config ./configs/cifar-resnet-shuffled_pixels.yaml
-python ./utils/train.py --config ./configs/cifar-resnet-partial_labels.yaml
